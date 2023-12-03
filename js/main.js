77327ee15c7a35378989572c93416f242b7f53e1
kenbugless <kenbugless@gmail.com>
2023-12-03 10:41:40 +0900

    버튼 클릭시 클릭한 색상값을 쿠키에 등록해서 body의 클래스로 지정

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
-/*
-1.사용자에게 보여줄 프리셋 컬러를 선택하는 모달 창 띄어줌
-2.사용자가 색상 선택시 해당 색상코드값을 받아서 color=색상값 형태로 1달 만료기한으로 쿠키 생성
-3.브라우저 접속시 해당 쿠키값으로 body의 클래스명을 변경해서 스타일 분기처리
-*/
-
 const colors = ['hotpink', 'cornflowerblue', 'violet', '#333'];
+const [btnColors, btnReset] = createPickerDOM(colors);
 
-const [btnColors, btnStnReset] = createPickerDOM(colors);
+//처음 로딩시 color= 시작하는 쿠키가 없으면 강제로 body요소의 클래스명을 제거
+if (document.cookie.indexOf('color=') < 0) {
+	document.body.className = '';
+} else {
+	//로딩시 color= 로 시작하는 쿠키가 있으면 해당 해당 문자열 다음에 있는 색상문자값만 잘라내기 해서
+	//body의 클래스로 지정
+	const pos = document.cookie.indexOf('color=');
+	const restCookie = document.cookie.slice(pos + 6);
+	const colorVal = restCookie.split(' ')[0];
+	document.body.className = colorVal;
+}
 
-btnColors.forEach((el, idx) => {
+btnColors.forEach((el) => {
 	el.addEventListener('click', (e) => {
 		const color = e.currentTarget.innerText;
 		console.log(color);
+		setCookie('color', color, 1);
+		document.body.className = color;
 	});
+});
+
+btnReset.addEventListener('click', () => {
+	const pos = document.cookie.indexOf('color=');
+	if (!pos < 0) {
+		const restCookie = document.cookie.slice(pos + 6);
+		console.log('rest', restCookie);
+		const colorVal = restCookie.split(' ')[0];
+		setCookie('color', colorVal, 0);
+	}
 });
 
 function createPickerDOM(arr) {
@@ -20,23 +37,21 @@ function createPickerDOM(arr) {
 	aside.classList.add('picker');
 	let tags = '';
 	arr.forEach((data) => {
-		tags += `<span style='background-color:${data}'> ${data} </span>`;
+		tags += `<span style='background-color:${data}'>${data}</span>`;
 	});
-
-	tags += `<button class="btnReset">컬러 초기화 </button>`;
+	tags += `<button class='btnReset'>컬러 초기화</button>`;
 
 	aside.innerHTML = tags;
 	document.body.append(aside);
-
 	return [document.querySelectorAll('aside span'), document.querySelector('aside .btnReset')];
 }
 
 function setCookie(name, value, expires) {
 	let now = new Date();
-	let duedate = now.getTime() + 1000 * 60 * 60 * expires; //1시간
+	let duedate = now.getTime() + 1000 * 60 * 60 * expires;
 	now.setTime(duedate);
-	document.cookie = `${name}=${value}; path=/; expires = ${now.toUTCString()}   `;
+	document.cookie = `${name}=${value}; path=/; expires= ${now.toUTCString()}`;
 }
 
-//미션1 = 각 버튼 클릭 시 사용자 컴퓨터에 1시간동안 유지되는 쿠키 생성
-//쿠키 형식: color = 클릭한 버튼의 생성 코드
+//미션1 - 각 버튼 클릭시 사용자 컴퓨터에 1시간동안 유지되는 쿠키 생성
+//쿠키 형식 : color=클릭한 버튼의 색상 코드
