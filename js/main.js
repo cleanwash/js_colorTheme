292132362f5f1f11451d8db8b826cbdb0fb25a24
kenbugless <kenbugless@gmail.com>
2023-12-03 09:39:36 +0900

    colorPicker 동적 생성

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@
 2.사용자가 색상 선택시 해당 색상코드값을 받아서 color=색상값 형태로 1달 만료기한으로 쿠키 생성
 3.브라우저 접속시 해당 쿠키값으로 body의 클래스명을 변경해서 스타일 분기처리
 */
+
+const colors = ['hotpink', 'cornflowerblue', 'violet', '#333'];
+
+createPickerDOM(colors);
+
+function createPickerDOM(arr) {
+	const aside = document.createElement('aside');
+	aside.classList.add('picker');
+	let tags = '';
+	arr.forEach((data) => {
+		tags += `<span style='background-color:${data}'> ${data} </span>`;
+	});
+
+	aside.innerHTML = tags;
+	document.body.append(aside);
+}
